bea41279d40ca13b6957857f69c76ec7323f2895
Sujay Chand <sujaygchand@gmail.com>
2021-09-23 02:15:26 +1200

    Cleaned up error handling and add an autobind decorator

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,18 @@
+/** Decorators */
+function Autobind(target: any, methodName: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value;
+
+    const adjDescriptor : PropertyDescriptor = {
+        configurable: true,
+
+        get(){
+            const boundFn = originalMethod.bind(this);
+            return boundFn;
+        }
+    };
+    return adjDescriptor;
+}
+
 class TaskInput{
     templateElement : HTMLTemplateElement;
     hostElement : HTMLDivElement;
@@ -8,14 +23,10 @@ class TaskInput{
 
     constructor() {
        this.templateElement = document.getElementById('task-input') as HTMLTemplateElement;
-       
-       if(this.templateElement === null)
-        throw new Error("task-input does not exist in index as HTMLTemplateElement");
+       this.checkHTMLElementIsValid([this.templateElement], "task-input does not exist in index as HTMLTemplateElement");
 
         this.hostElement = document.getElementById('app') as HTMLDivElement; 
-        
-        if(this.hostElement === null)
-         throw new Error("app does not exist in index as HTMLDivElement");
+       this.checkHTMLElementIsValid([this.hostElement], "app does not exist in index as HTMLDivElement");
 
         const importedNode = document.importNode(this.templateElement.content, true);
 
@@ -24,16 +35,13 @@ class TaskInput{
         this.titleInputElement = this.formElement.querySelector("#title") as HTMLInputElement;
         this.descriptionInputElement = this.formElement.querySelector("#description") as HTMLInputElement;
         this.effortInputElement = this.formElement.querySelector("#effort") as HTMLInputElement;
-        
-
-        if(!this.titleInputElement || !this.descriptionInputElement || !this.effortInputElement)
-            throw new Error("Input element does not have correct id assigned");
-
+        this.checkHTMLElementIsValid([this.titleInputElement, this.descriptionInputElement, this.effortInputElement], "An input element does not have correct id assigned");
 
         this.configure();
         this.attach();
     }
 
+    @Autobind
     private submitHandler(event : Event) {
         event.preventDefault();
         console.log(this.titleInputElement.value);
@@ -41,11 +49,19 @@ class TaskInput{
     }
 
     private configure() {
-        this.formElement.addEventListener("submit", this.submitHandler.bind(this));
+        this.formElement.addEventListener("submit", this.submitHandler);
     }
 
     private attach() {
         this.hostElement.insertAdjacentElement("afterbegin", this.formElement);
+    }
+
+    private checkHTMLElementIsValid(elements : HTMLElement[], errorMessage : string) {
+        for(const element in elements){
+            
+            if(element === null)
+            throw new Error(errorMessage);
+        }
     }
 }
 
